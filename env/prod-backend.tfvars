e3c3a54541569af207d48f7f336cefc7ba8c4f8c
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-02 19:46:17 -0700

    updating ./terraform/roboshop-terraform-mutable

---
+++ env/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-kvbucket"
+key    = "terraform/mutable/prod/terraform.tfstate"
+region = "us-east-1"
--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 module "VPC" {
   source = "github.com/krishnavamsi7616/tf-module-vpc"
+  ENV = var.ENV
+  PROJECT = var.PROJECT
+  VPC_CIDR = var.VPC_CIDR
 }
